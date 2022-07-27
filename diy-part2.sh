8123b2b577962a2b1855305bb40f53f1b12f29fa
silicer <ywx981004@gmail.com>
2022-07-28 07:48:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,9 +18,10 @@ echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config; make defconfig
 # curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/logo.jpg > .package/luci-app-serverchan/root/usr/bin/serverchan/api/logo.jpg
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-# rm -rf ./package/lean/luci-app-qbittorrent
-# rm -rf ./package/lean/luci-theme-argon
-# rm -rf ./package/lean/luci-app-serverchan
+rm -rf ./feeds/packages/net/mosdns
+rm -rf ./package/lean/luci-app-qbittorrent
+rm -rf ./package/lean/luci-theme-argon
+rm -rf ./package/lean/luci-app-serverchan
 # rm -rf ./feeds/packages/net/adguardhome
 # rm -rf ./feeds/packages/net/smartdns
 # rm -rf ./feeds/packages/net/https-dns-proxy
