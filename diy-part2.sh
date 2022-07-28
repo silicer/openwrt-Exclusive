0d4280259e7bc28a2a64f0c193f55d972d533fa0
silicer <ywx981004@gmail.com>
2022-07-28 23:53:58 +0800

    不整这些花里胡哨的了

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,7 +10,7 @@
 #============================================================
 sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
 sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
-echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config; make defconfig
+# echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config; make defconfig
 # sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt-Firker '/g" ./package/lean/default-settings/files/zzz-default-settings
 # sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-SE'/g" ./package/base-files/files/bin/config_generate
 # curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
@@ -18,12 +18,12 @@ echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config; make defconfig
 # curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/logo.jpg > .package/luci-app-serverchan/root/usr/bin/serverchan/api/logo.jpg
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-rm -rf ./feeds/packages/net/mosdns
-rm -rf ./package/feeds/kenzo/luci-app-mosdns
-rm -rf ./package/feeds/kenzo/mosdns
-rm -rf ./package/lean/luci-app-qbittorrent
-rm -rf ./package/lean/luci-theme-argon
-rm -rf ./package/lean/luci-app-serverchan
+# rm -rf ./feeds/packages/net/mosdns
+# rm -rf ./package/feeds/kenzo/luci-app-mosdns
+# rm -rf ./package/feeds/kenzo/mosdns
+# rm -rf ./package/lean/luci-app-qbittorrent
+# rm -rf ./package/lean/luci-theme-argon
+# rm -rf ./package/lean/luci-app-serverchan
 # rm -rf ./feeds/packages/net/adguardhome
 # rm -rf ./feeds/packages/net/smartdns
 # rm -rf ./feeds/packages/net/https-dns-proxy
@@ -32,8 +32,8 @@ rm -rf ./package/lean/luci-app-serverchan
 # svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
 
 # MosDNS
-svn co https://github.com/QiuSimons/openwrt-mos/trunk/luci-app-mosdns package/luci-app-mosdns
-svn co https://github.com/QiuSimons/openwrt-mos/trunk/mosdns feeds/packages/net/mosdns
+# svn co https://github.com/QiuSimons/openwrt-mos/trunk/luci-app-mosdns package/luci-app-mosdns
+# svn co https://github.com/QiuSimons/openwrt-mos/trunk/mosdns feeds/packages/net/mosdns
 # svn co https://github.com/QiuSimons/openwrt-mos/trunk/v2ray-geodata package/v2ray-geodata
 
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-clash package/luci-app-clash
